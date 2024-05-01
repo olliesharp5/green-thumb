b3b03490506a241069e441e22d5722c63f4808fa
Oliver Sharp <olliesharp5@gmail.com>
2024-05-01 10:09:05 +0000

    Added templates, models and views to products app

--- green_thumb/urls.py
+++ green_thumb/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    # path('products/', include('products.urls')),
+    path('products/', include('products.urls')),
     # path('services/', include('services.urls')),
     # path('blog/', include('blog.urls')),
 ]
--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    image = models.ImageField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
