b5144e9e2615e6ea68bf085d803f828176fbe698
Oliver Sharp <olliesharp5@gmail.com>
2024-05-01 10:16:37 +0000

    Amended view to handle search queries

--- products/views.py
+++ products/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from .models import Product
 
 def all_products(request):
-    products = Product.objects.all()
+    query = request.GET.get('q')
+    if query:
+        products = Product.objects.filter(name__icontains=query)
+    else:
+        products = Product.objects.all()
     return render(request, 'products/products.html', {'products': products})
