a7788adaedeff3b97f80e823146282f0de7c72e3
Oliver Sharp <olliesharp5@gmail.com>
2024-05-01 10:48:32 +0000

    Added content, views and urls to products app

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
@@ -8,3 +8,12 @@ def all_products(request):
     else:
         products = Product.objects.all()
     return render(request, 'products/products.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
