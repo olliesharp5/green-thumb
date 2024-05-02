fb9e2af8e4d582311f5da5fe1314b848fac3c904
Oliver Sharp <olliesharp5@gmail.com>
2024-05-02 15:42:49 +0000

    Wired up profile page

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,9 @@ from .models import UserProfile, Wishlist
 
 # Create your views here.
 
+def profile(request):
+    return render(request, 'profiles/profile.html')
+
 def add_to_wishlist(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     user_profile = UserProfile.objects.get(user=request.user)
