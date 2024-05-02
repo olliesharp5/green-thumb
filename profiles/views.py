f94acb1b156b51aae2209f1e65948a80c64df1f4
Oliver Sharp <olliesharp5@gmail.com>
2024-05-02 16:19:36 +0000

    Wired up wishlist to display on profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from products.models import Product
 from .models import UserProfile, Wishlist
 
 # Create your views here.
 
+@login_required
 def profile(request):
-    return render(request, 'profiles/profile.html')
+    user_profile = UserProfile.objects.get(user=request.user)
+    wishlist = Wishlist.objects.get(user=user_profile)
+    context = {
+        'wishlist': wishlist,
+    }
+    return render(request, 'profiles/profile.html', context)
 
 def add_to_wishlist(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
