5dbffb0d7ba69c0a2d591a2581699222b4bef041
Oliver Sharp <olliesharp5@gmail.com>
2024-05-06 17:35:22 +0000

    Added add_to_cart view and tested functionality

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cart_view, name='cart_view')
+    path('', views.cart_view, name='cart_view'),
+    path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from products.models import Product
 
 # Create your views here.
 def cart_view(request):
     """ A view to return the cart.html page """
     return render(request, "cart/cart.html")
+
+
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    cart[str(product_id)] = cart.get(str(product_id), 0) + quantity
+    request.session['cart'] = cart
+    print(request.session['cart'])  # print the cart
+    return redirect('product_detail', product_id=product_id)
