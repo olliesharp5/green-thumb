fc41c7381dfb0d957a7927ddeb23846b98136bc0
Oliver Sharp <olliesharp5@gmail.com>
2024-05-06 18:09:20 +0000

    Added has_size to product model and template

--- products/models.py
+++ products/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_size = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True, validators=[MinValueValidator(0), MaxValueValidator(5)])
     image = models.ImageField(null=True, blank=True)
