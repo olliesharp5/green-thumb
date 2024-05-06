9ab7bdaef75d3179a253ec89ad6bb5bcb36f4bdd
Oliver Sharp <olliesharp5@gmail.com>
2024-05-06 18:54:03 +0000

    Amended view to handle adding sizes to cart

--- cart/contexts.py
+++ cart/contexts.py
@@ -10,17 +10,30 @@ def cart_contents(request):
     total = 0
     product_count = 0
 
-    for product_id, quantity in cart.items():
+    for product_id, item_data in cart.items():
         product = get_object_or_404(Product, pk=product_id)
-        item_total = quantity * product.price
-        total += item_total
-        product_count += quantity
-        cart_items.append({
-            'product_id': product_id,
-            'quantity': quantity,
-            'product': product,
-            'total': item_total,
-        })
+        if isinstance(item_data, int):
+            item_total = item_data * product.price
+            total += item_total
+            product_count += item_data
+            cart_items.append({
+                'product_id': product_id,
+                'quantity': item_data,
+                'product': product,
+                'total': item_total,
+            })
+        else:
+            for size, quantity in item_data['items_by_size'].items():
+                item_total = quantity * product.price
+                total += item_total
+                product_count += quantity
+                cart_items.append({
+                    'product_id': product_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'total': item_total,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- cart/views.py
+++ cart/views.py
@@ -11,8 +11,24 @@ def cart_view(request):
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     cart = request.session.get('cart', {})
-    cart[str(product_id)] = cart.get(str(product_id), 0) + quantity
+
+    if size:
+        if str(product_id) in list(cart.keys()):
+            if size in cart[str(product_id)]['items_by_size'].keys():
+                cart[str(product_id)]['items_by_size'][size] += quantity
+            else:
+                cart[str(product_id)]['items_by_size'][size] = quantity
+        else:
+            cart[str(product_id)] = {'items_by_size': {size: quantity}}
+    else:
+        if str(product_id) in list(cart.keys()):
+            cart[str(product_id)] += quantity
+        else:
+            cart[str(product_id)] = quantity
+
     request.session['cart'] = cart
-    
     return redirect('product_detail', product_id=product_id)
