4bd9e2d2bb2081e3f56b44b671942ea0473f838a
Oliver Sharp <olliesharp5@gmail.com>
2024-05-06 19:01:23 +0000

    Added buttons to increment quantity value

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,17 @@ function goBack() {
     window.history.back();
   }
 
+  function incrementValue() {
+    var value = parseInt(document.getElementById('quantity').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('quantity').value = value;
+}
+
+function decrementValue() {
+    var value = parseInt(document.getElementById('quantity').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('quantity').value = value;
+}
