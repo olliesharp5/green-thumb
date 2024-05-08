1439bfc764cec1d804e4d959d0048e0dba60f720
Oliver Sharp <olliesharp5@gmail.com>
2024-05-08 13:16:32 +0000

    Updated admin.py to display information in admin panel

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'grand_total', 'view_line_items')
+
+    ordering = ('-date',)
+    
+    search_fields = ['full_name', 'email', 'order_number']
+    list_filter = ['date', 'delivery_cost', 'country']
+
+    def view_line_items(self, obj):
+        return "\n".join([str(line_item.product) for line_item in obj.lineitems.all()])
+
+    view_line_items.short_description = 'Line Items'
+
+admin.site.register(Order, OrderAdmin)
