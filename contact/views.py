113db05202b3660401c3188d4369acc752e7d6b0
Oliver Sharp <olliesharp5@gmail.com>
2024-05-16 17:37:45 +0000

    Added functionality to send email when submitting CS form

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 from .models import ContactRequest
 
 def contact(request):
@@ -18,6 +21,24 @@ def contact(request):
             file_upload=file_upload
         )
         contact_request.save()
+
+        # Prepare email
+        email_subject = render_to_string(
+            'contact/confirmation_emails/confirmation_email_subject.txt',
+            {'contact_request': contact_request}
+        )
+        email_body = render_to_string(
+            'contact/confirmation_emails/confirmation_email_body.txt',
+            {'contact_request': contact_request, 'contact_email': settings.DEFAULT_FROM_EMAIL}
+        )
+
+        # Send confirmation email
+        send_mail(
+            email_subject,
+            email_body,
+            settings.DEFAULT_FROM_EMAIL,
+            [email]
+        )
 
         messages.success(request, 'Your contact request has been submitted. View your requests in "My Profile"')
         return redirect('contact')
