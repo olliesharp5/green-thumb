c5e4a6687fb8113c34281564fbbee4c9a5859613
Oliver Sharp <olliesharp5@gmail.com>
2024-05-20 20:27:16 +0000

    Moved image files and edited aws settings for production

--- green_thumb/settings.py
+++ green_thumb/settings.py
@@ -189,7 +189,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if os.environ.get('USE_AWS') == 'True':
+if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
