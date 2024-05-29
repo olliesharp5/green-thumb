6abb6d262d7ab42ff9e89160faab54695a3eca04
Oliver Sharp <olliesharp5@gmail.com>
2024-05-29 17:35:11 +0000

    Amended formatting in register and login forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,10 +2,22 @@ from django import forms
 from django.contrib.auth.models import User
 from profiles.models import UserProfile
 
+from django import forms
+from django.contrib.auth.models import User
+
 class RegistrationForm(forms.ModelForm):
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    gardener = forms.BooleanField(required=False, widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        label='Confirm password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    gardener = forms.BooleanField(
+        required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
 
     class Meta:
         model = User
@@ -23,6 +35,7 @@ class RegistrationForm(forms.ModelForm):
         if commit:
             user.save()
         return user
+
 
 class GardenerForm(forms.ModelForm):
     display_name = forms.CharField(max_length=100, required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'id': 'display_name'}))
