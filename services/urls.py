9d7b96ce3bd88d5b23d0ad80d3ef02a72f6e8bc9
Oliver Sharp <olliesharp5@gmail.com>
2024-06-03 14:52:05 +0000

    Added further conditionals to feedback edit, delete and create

--- services/urls.py
+++ services/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('gardener/<str:username>/', views.gardener_profile, name='gardener_profile'),
     path('gardener_feedback/', views.gardener_feedback, name='gardener_feedback'),
     path('gardener_feedback/<int:feedback_id>/update/', views.update_gardener_feedback, name='update_gardener_feedback'),
-path('gardener_feedback/<int:feedback_id>/delete/', views.delete_gardener_feedback, name='delete_gardener_feedback'),
+    path('gardener_feedback/<int:feedback_id>/delete/', views.delete_gardener_feedback, name='delete_gardener_feedback'),
     path('service_request/', views.service_request, name='service_request'),
 ]
